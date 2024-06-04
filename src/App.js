033b33d245795853f7b3a3611661d333788031bc
mahongri <mahongri1993@gmail.com>
2024-06-03 22:38:13 -0400

    hook usememo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { useMemo } from 'react';
+// 缓存： 消耗非常大的计算结果，然后缓存起来，下次直接使用缓存的结果
+function fib(n) {
+  if (n < 3)
+    return 1;
+  return fib(n - 1) + fib(n - 2);
+}
 
 function App() {
+  const [count1, setCount1] = useState(0);
+  const res = useMemo(() => {
+    // 返回计算的结果
+    return fib(count1)
+  }, [count1])
+  // const res = fib(count1)
+  const [count2, setCount2] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick = { () => {
+        setCount1(count1 + 1);
+      }}>change count1: { count1 }</button>
+
+      <button onClick = { () => {
+              setCount2(count2 + 1);
+            }}>change count2: { count2 }</button>
+
+      { res}
+
     </div>
   );
 }
